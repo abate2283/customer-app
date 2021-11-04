87a822843d056eefb7b2a3911d9d361425b67933
Alfred A Bate <alfredbate@yahoo.co.uk>
2021-11-04 09:57:06 -0500

    committed changes to dev branch

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ function App() {
       <p>
         <h1>  Welcome to our Website.  Work in progress, come back later
           <table>
+            <thead>
             <tr>
               <th>Girsl</th>
               <th>Boys</th>
               <th>Kids</th>
               <th>Toys</th>
             </tr>
+            </thead>
+            <tbody>
             <tr>
               <td>Things</td>
               <td>Houses</td>
               <td>Devours</td>
               <td>Deep</td>
             </tr>
+            </tbody>
           </table>
         </h1>
       </p>
