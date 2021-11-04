2a1145a6cd96d7d42401ff6e63e109a766eff0ed
Alfred A Bate <alfredbate@yahoo.co.uk>
2021-11-04 09:40:16 -0500

    modified app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ function App() {
   return (
     <div className="App">
       <p>
-        <h1>  Welcome to our Website.  Work in progress, come back later</h1>
+        <h1>  Welcome to our Website.  Work in progress, come back later
+          <table>
+            <tr>
+              <th>Girsl</th>
+              <th>Boys</th>
+              <th>Kids</th>
+              <th>Toys</th>
+            </tr>
+            <tr>
+              <td>Things</td>
+              <td>Houses</td>
+              <td>Devours</td>
+              <td>Deep</td>
+            </tr>
+          </table>
+        </h1>
       </p>
     </div>
   );
